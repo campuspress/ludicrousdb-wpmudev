e92d92ad73daef8f8730962489dd4fa1899c4cf3
vladislav <ve@incsub.com>
2023-03-11 07:45:17 +0100

    Add some docs and fix linter recommendations

--- ludicrousdb/includes/sharding.php
+++ ludicrousdb/includes/sharding.php
@@ -1,19 +1,43 @@
 <?php
+/**
+ * Custom network sharding setup with LudicrousDB.
+ * @phpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound
+ */
 
+/**
+ * Global constants setup
+ */
 final class MultisiteDataset_Config {
 	const GLOBAL_DATASET = 'global';
 	const GLOBAL_READER  = 'global__r';
 	const DATASET_FIELD  = 'srv';
 }
 
+/**
+ * Shard distribution
+ * Used in new site creation
+ */
 class MultisiteDataset_Sharder {
 
+	/**
+	 * WPDB
+	 *
+	 * @var LudicrousDB
+	 */
 	private $_db;
 
+	/**
+	 * Constructor
+	 *
+	 * @param object $db LudicrousDB instance
+	 */
 	public function __construct( object $db ) {
 		$this->_db = $db;
 	}
 
+	/**
+	 * Gets a list of registered shards
+	 */
 	public function get_shards() {
 		return array_values(
 			array_filter(
@@ -25,12 +49,22 @@ class MultisiteDataset_Sharder {
 		);
 	}
 
+	/**
+	 * Distributes a site among registered shards
+	 *
+	 * @param int $blog_id Site ID
+	 */
 	public function shard_for( int $blog_id ): string {
 		$blog_id = (int) $blog_id;
 		$shards  = $this->get_shards();
 		return $shards[ $blog_id % count( $shards ) ];
 	}
 
+	/**
+	 * Whether or not we have a valid, registered shard
+	 *
+	 * @param string $shard Shard name
+	 */
 	public function is_valid_shard( string $shard ): bool {
 		if ( $shard === MultisiteDataset_Config::GLOBAL_DATASET ) {
 			return true;
@@ -39,17 +73,46 @@ class MultisiteDataset_Sharder {
 	}
 }
 
+/**
+ * Main dataset handler class
+ */
 class MultisiteDataset {
+
+	/**
+	 * WPDB
+	 *
+	 * @var LudicrousDB
+	 */
 	private $_db;
+
+	/**
+	 * Selector
+	 *
+	 * @var MultisiteDataset_QuerySelector
+	 */
 	private $_selector;
+
+	/**
+	 * Shard dispatcher
+	 *
+	 * @var MultisiteDataset_Sharder
+	 */
 	private $_sharder;
 
+	/**
+	 * Constructor
+	 *
+	 * @param object $db LudicrousDB instance
+	 */
 	public function __construct( object $db ) {
 		$this->_db       = $db;
 		$this->_selector = new MultisiteDataset_QuerySelector();
 		$this->_sharder  = new MultisiteDataset_Sharder( $db );
 	}
 
+	/**
+	 * Register callbacks and listen to WP hooks
+	 */
 	public function init() {
 		$this->add_query_selector();
 
@@ -59,6 +122,11 @@ class MultisiteDataset {
 		);
 	}
 
+	/**
+	 * Handles new network site creation
+	 *
+	 * @param object $site WordPress site object: WP_Site instance
+	 */
 	public function handle_insert_site( object $site ) {
 		$this->remove_query_selector();
 		$shard = $this->_sharder->shard_for( $site->id );
@@ -70,14 +138,23 @@ class MultisiteDataset {
 		$this->add_query_selector();
 	}
 
+	/**
+	 * Gets the sharding query selector instance
+	 */
 	public function get_selector(): MultisiteDataset_QuerySelector {
 		return $this->_selector;
 	}
 
+	/**
+	 * Gets the actual sharding callback
+	 */
 	public function get_callback(): callable {
 		return array( $this->get_selector(), 'query_select' );
 	}
 
+	/**
+	 * Registers query selector callback
+	 */
 	public function add_query_selector() {
 		$this->_db->add_callback(
 			$this->get_callback(),
@@ -85,6 +162,9 @@ class MultisiteDataset {
 		);
 	}
 
+	/**
+	 * Deregisters query selector callback
+	 */
 	public function remove_query_selector(): bool {
 		$nerf = false;
 		foreach ( $this->_db->ludicrous_callbacks['dataset'] as $idx => $cback ) {
@@ -101,29 +181,67 @@ class MultisiteDataset {
 	}
 }
 
+/**
+ * Handles DB queries
+ */
 class MultisiteDataset_QuerySelector {
+
+	/**
+	 * Local datasets cache
+	 *
+	 * @var array
+	 */
 	private $_datasets = array();
 
+	/**
+	 * Checks whether we have a dataset cached locally
+	 *
+	 * @param int  $bid Site ID
+	 * @param bool $empty_check Whether to validate for presence
+	 */
 	public function has_dataset( int $bid, bool $empty_check = false ): bool {
 		return empty( $empty_check )
 			? isset( $this->_datasets[ $bid ] )
 			: ! empty( $this->_datasets[ $bid ] );
 	}
 
+	/**
+	 * Gets local dataset cache
+	 *
+	 * @param int $bid Site ID
+	 */
 	public function get_dataset( int $bid ): string {
 		return $this->_datasets[ $bid ];
 	}
 
+	/**
+	 * Sets local dataset cache for a site
+	 *
+	 * @param int    $bid Site ID
+	 * @param string $hndl Dataset
+	 */
 	public function set_dataset( int $bid, string $hndl ) {
 		$this->_datasets[ $bid ] = $hndl;
 	}
 
+	/**
+	 * Clears local dataset cache
+	 *
+	 * @param int $bid Site ID
+	 */
 	public function unset_dataset( int $bid ) {
 		if ( $this->has_dataset( $bid ) ) {
 			unset( $this->_datasets[ $bid ] );
 		}
 	}
 
+	/**
+	 * Update shard info in DB for a particular site
+	 *
+	 * @param int         $blog_id Site ID to update
+	 * @param string      $shard Shard (dataset) name
+	 * @param LudicrousDB $wpdb LudicrousDB instance
+	 */
 	public function shard_update( $blog_id, $shard, $wpdb ): bool {
 		$global = MultisiteDataset_Config::GLOBAL_READER;
 		$dbh    = $wpdb->dbhs[ $global ];
@@ -149,7 +267,13 @@ class MultisiteDataset_QuerySelector {
 		return ! empty( $result );
 	}
 
-	// TODO: refactor
+	/**
+	 * LudicrousDB dataset handler callback
+	 * TODO: refactor
+	 *
+	 * @param string      $query MySQL query
+	 * @param LudicrousDB $wpdb LudicrousDB instance
+	 */
 	public function query_select( $query, $wpdb ) {
 		if ( empty( $wpdb->dbhname ) ) {
 			return;
